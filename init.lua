f329328f6d5182c331d9b06cbf399b39e8c2c264
wanto-production <ikwansatria3974@gmail.com>
2025-04-11 16:15:42 +0700

    update commit

--- init.lua
+++ init.lua
@@ -1,2 +1,19 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
+
+--- for wayland clipboard support
+vim.opt.clipboard = "unnamedplus"
+
+vim.g.clipboard = {
+  name = "wl-clipboard",
+  copy = {
+    ["+"] = "wl-copy --foreground --type text/plain",
+    ["*"] = "wl-copy --foreground --type text/plain",
+  },
+  paste = {
+    ["+"] = "wl-paste --no-newline",
+    ["*"] = "wl-paste --no-newline",
+  },
+  cache_enabled = false,
+}
+
 require("config.lazy")
